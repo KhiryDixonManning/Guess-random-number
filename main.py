a25bfad8e21f4e75919e4e3d0e14f75ae58e4ee2
KhiryDixonManning <Manningkhiry@gmail.com>
2024-11-29 16:39:19 -0600

    added ability for computer to guess number

--- main.py
+++ main.py
@@ -12,4 +12,24 @@ def guess(x):
         
     print('Yay, nice work!! you have guessed the number {random_number} correctly')
 
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+         guess = random.randint(low, high)
+    else:
+        guess = low # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess +1
+            
+        print('Yay! The computer guessed correctly, it was {guess}')
+
+
+
 guess(10)
