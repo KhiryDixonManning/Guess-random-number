9c5bf963baa329a78d00a2fda644b9b215b167ab
KhiryDixonManning <Manningkhiry@gmail.com>
2024-12-04 17:50:01 -0600

    made some small changes  no longer infinitely loops

--- main.py
+++ main.py
@@ -2,34 +2,44 @@ import random
 
 def guess(x):
     random_number = random.randint(1, x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}'))
-        if guess < random_number:
-            print('Sorry, try again too low.')
-        elif guess >random_number:
-            print('Sorry, try again too high')
-        
-    print('Yay, nice work!! you have guessed the number {random_number} correctly')
-
+    while True:
+        try:
+            guess = int(input(f'Guess a number between 1 and {x}: '))
+            if guess < random_number:
+                print('Sorry, try again. Too low.')
+            elif guess > random_number:
+                print('Sorry, try again. Too high.')
+            else:
+                print(f'Yay, nice work!! You have guessed the number {random_number} correctly')
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
 
 def computer_guess(x):
     low = 1
     high = x
-    feedback = ''
-    while feedback != 'c':
-        if low != high:
-         guess = random.randint(low, high)
-    else:
-        guess = low # could also be high b/c low = high
-        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+    print(f"Think of a number between 1 and {x}")
+    
+    while True:
+        if low == high:
+            guess = low
+        else:
+            guess = random.randint(low, high)
+        
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)? ').lower()
+        
         if feedback == 'h':
-            high = guess -1
+            high = guess - 1
         elif feedback == 'l':
-            low = guess +1
-            
-        print('Yay! The computer guessed correctly, it was {guess}')
+            low = guess + 1
+        elif feedback == 'c':
+            print(f'Yay! The computer guessed correctly, it was {guess}')
+            break
+        
+        if low > high:
+            print("Something went wrong. The number cannot be found.")
+            break
 
-
-
-computer_guess(1000)
+# Uncomment the function you want to play
+# guess(1000)
+# computer_guess(1000)
